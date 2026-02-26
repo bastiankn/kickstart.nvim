ce4faff426d5c0437c87987300db2b7eba2eb90c
bastiankn <bastian@bastian-T14U.fritz.box>
2026-02-26 11:44:06 +0100

    Bug fix rust_analyzer

--- lua/kickstart/plugins/lsp.lua
+++ lua/kickstart/plugins/lsp.lua
@@ -122,13 +122,16 @@ return{
       --    :Mason
       --
       -- You can press `g?` for help in this menu.
-      local ensure_installed = vim.tbl_keys(servers or {})
-      vim.list_extend(ensure_installed, {
+      -- NOTE: Use Mason tool names (not lspconfig names) for ensure_installed
+      -- local ensure_installed = vim.tbl_keys(servers or {})
+      -- vim.list_extend(ensure_installed, {
+      -- E.g., 'rust-analyzer' (hyphen) for Mason, but 'rust_analyzer' (underscore) in servers table
+      local ensure_installed = {
         'lua-language-server', -- Lua Language server
         'stylua', -- Used to format Lua code
         'rust-analyzer', -- Rust language server
         -- You can add other tools here that you want Mason to install
-      })
+      }
 
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
