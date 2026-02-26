17c385d839bf75ac2a9bd598d3cfb10c1903ec36
bastiankn <bastian@bastian-T14U.fritz.box>
2026-02-26 10:27:24 +0100

    update comments

--- lua/custom/core/options.lua
+++ lua/custom/core/options.lua
@@ -1,10 +1,12 @@
+-- [[ Leader Key]]
 -- Set <space> as the leader key
 -- See `:help mapleader`
---  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+-- [[ UI & Display ]]
 -- Set to true if you have a Nerd Font installed and selected in the terminal
+-- Will use fallback icons if false
 vim.g.have_nerd_font = false
 
 -- Make line numbers default
@@ -12,22 +14,22 @@ vim.o.number = true
 -- relative line numbers default remove below
 -- vim.o.relativenumber = true
 
--- Enable mouse mode, can be useful for resizing splits for example!
+-- Enable mouse mode, useful for resizing, splits, etc.
+-- See `:help 'mouse'` 
 vim.o.mouse = 'a'
 
 -- Don't show the mode, since it's already in the status line
 vim.o.showmode = false
 
 -- Sync clipboard between OS and Neovim.
---  Schedule the setting after `UiEnter` because it can increase startup-time.
---  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
 vim.schedule(function() vim.o.clipboard = 'unnamedplus' end)
 
+-- [[ Editing Behavior ]]
 -- Enable break indent
 vim.o.breakindent = true
 
--- Save undo history
+-- Save undo history - even after closing the editor
 vim.o.undofile = true
 
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
@@ -43,10 +45,12 @@ vim.o.updatetime = 250
 -- Decrease mapped sequence wait time
 vim.o.timeoutlen = 300
 
+-- [[ Window Split ]]
 -- Configure how new splits should be opened
 vim.o.splitright = true
 vim.o.splitbelow = true
 
+-- [[ Visual Helper ]]
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
 --  and `:help 'listchars'`
@@ -64,11 +68,10 @@ vim.o.inccommand = 'split'
 -- Show which line your cursor is on
 vim.o.cursorline = true
 
--- Minimal number of screen lines to keep above and below the cursor.
+-- Keep x lines visible when scrolling vertically
 vim.o.scrolloff = 10
 
--- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
--- instead raise a dialog asking if you wish to save the current file(s)
+-- Show save dialog instead of errors when trying to quit with unsaved changes
 -- See `:help 'confirm'`
 vim.o.confirm = true
 
